f9d86b509dc36ec76bb00697980a396ff370b54b
Brandon Chen <parkghost@gmail.com>
2013-04-08 13:06:18 +0800

    reduce memory pressure

--- benchmark.go
+++ benchmark.go
@@ -27,7 +27,7 @@ func NewBenchmark(config *Config, start *sync.WaitGroup, stop chan bool) *Benchm
 
 func (b *Benchmark) Run() {
 
-	jobs := make(chan *http.Request, b.config.requests)
+	jobs := make(chan *http.Request, b.config.concurrency*GoMaxProcs)
 
 	for i := 0; i < b.config.concurrency; i++ {
 		go NewHttpWorker(b.config, b.start, b.stop, jobs, b.collector).Run()
