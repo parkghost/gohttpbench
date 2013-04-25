5d4483ce2b1d4cae835dcaa618ed8322dddfdfa5
Brandon Chen <parkghost@gmail.com>
2013-04-26 07:44:30 +0800

    fix malfunction of stopwatch unit test and reduce testing time

--- common_test.go
+++ common_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 func TestStopWatch(t *testing.T) {
-	testData := []int{1, 2}
+	testData := []int{100, 200} //Millisecond
 	for _, value := range testData {
 		sw := &StopWatch{}
 
 		sw.Start()
-		time.Sleep(time.Duration(value) * time.Second)
+		time.Sleep(time.Duration(value) * time.Millisecond)
 		sw.Stop()
 
-		if int64(sw.Elapsed)/1000000 == int64(time.Duration(value)*time.Second) {
-			t.Errorf("expected %d, get %d", time.Duration(value)*time.Second, sw.Elapsed)
+		if int64(sw.Elapsed)/1000000 != int64(time.Duration(value)) {
+			t.Errorf("expected %d, get %d", time.Duration(value)*time.Millisecond, sw.Elapsed)
 		}
 	}
 
